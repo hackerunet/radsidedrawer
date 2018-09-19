e5c5ef1bdbf0beacc80b6bc57b633e2cd4cee3c6
Luis Parada <lparada@reliqhealth.com>
2018-09-19 16:59:08 -0500

    Added main app outlet with default login route

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { LoginComponent } from "./login/login.component";
 import { MenuComponent } from "./menu/menu.component";
 
 const routes: Routes = [
-    { path: "", redirectTo: "/login", pathMatch: "full" },
-    { path: "login", component: LoginComponent },
-    { path: "menu", component: MenuComponent, children: [
+    { path: "", redirectTo: "/(app:login)", pathMatch: "full" },
+    { path: "login", component: LoginComponent, outlet: "app" },
+    { path: "menu", component: MenuComponent, outlet: "mainapp", children: [
             { path: "", redirectTo: "home", pathMatch: "full" },
             { path: "home", loadChildren: "./home/home.module#HomeModule" },
             { path: "browse", loadChildren: "./browse/browse.module#BrowseModule" },
